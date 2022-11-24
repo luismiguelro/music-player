4ce59d1475da821341f7631b597d553214c5f4c0
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2022-11-23 19:50:27 -0500

    functions play next and prev

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/*Objetivo: llevar acabo cada una de las funciones correspondientes desglozadas, 
+aunque puede tener mejoras en el codigo */
+
 // Song data
 const songList = [
     {
@@ -26,6 +29,22 @@ const songs = document.getElementById("songs");
 const audio = document.getElementById("audio");
 const cover = document.getElementById("cover");
 const title = document.getElementById("title");
+const play = document.getElementById("play");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+
+
+// eventos controles
+play.addEventListener("click",()=>{
+   if(audio.paused){
+        playSong();
+    } else if(audio.play){
+        pausedSong();
+    }
+});
+
+next.addEventListener("click",()=>nextSong());
+prev.addEventListener("click",()=>prevSong());
 
 function loadSongs() {
     //index, saber que cancion reproducir
@@ -51,11 +70,11 @@ function loadSong (songIndex){
     
     // cancion actual
     if (songIndex !== actualSong){
+        changeActiveClass(actualSong,songIndex)
         actualSong= songIndex;
         // cambiar fuente del audio
         audio.src = "./audio/"+ songList[songIndex].file;
-        audio.play();
-
+        playSong();
         changeCover(songIndex);
         changeSongTitle(songIndex);
     }
@@ -63,8 +82,41 @@ function loadSong (songIndex){
 
 }
 
-//  cancion actual
+//Actualizar controles
+function updateControls(){
+    if(audio.paused){
+        play.classList.remove("fa-pause");
+        play.classList.add("fa-play");
 
+    }else{
+        play.classList.add("fa-pause");
+        play.classList.remove("fa-play");
+    }
+
+}
+
+//reproducir cancion
+function playSong(){
+    if(actualSong !== null){
+    audio.play();
+    updateControls();
+    }
+}
+
+// pausar cancion
+function pausedSong(){
+    audio.pause();
+    updateControls();
+}
+
+//  cambiar clase activa 
+function changeActiveClass(lastIndex, newIndex) {
+    const links = document.querySelectorAll("a");
+    if (lastIndex !== null) {
+        links[lastIndex].classList.remove("active");
+    }
+    links[newIndex].classList.add("active");
+}
 
 // Cambiar cover cancion
 function changeCover(songIndex){
@@ -76,6 +128,19 @@ function changeCover(songIndex){
 function changeSongTitle(songIndex){
     title.innerText = songList[songIndex].title;
 }
+//anterior cancion
+function prevSong(){
+    loadSong(actualSong-1);
+}
 
-
+//siguiente cancion
+function nextSong(){
+    if(actualSong<songList.length-1){
+        loadSong(actualSong+1);
+    } else{
+        loadSong(0);
+    }
+    
+}
+//GO
 loadSongs();
